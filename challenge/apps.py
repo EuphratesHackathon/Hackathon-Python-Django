f77d5da7151ff53fe8c8b24023c05833872c812a
Olanrewaju Abdulbasit Gbolahan <gbolahanbasit43@gmail.com>
2025-04-19 20:57:39 +0100

    Revert "Yusasive"

--- challenge/apps.py
+++ challenge/apps.py
@@ -4,9 +4,3 @@ from django.apps import AppConfig
 class ChallengeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'challenge'
-    verbose_name = "CTF Challenges"
-
-    def ready(self):
-        # Import signals or initialization logic here if needed
-        # from . import signals
-        pass
--- challenge/models.py
+++ challenge/models.py
@@ -1,59 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-import hashlib
-
+import hashlib # Import hashlib
+from django.core.exceptions import ValidationError  # Import ValidationError
 
 class Challenge(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     docker_image = models.CharField(max_length=100, unique=True)
-    docker_port = models.PositiveIntegerField()
-    start_port = models.PositiveIntegerField()
-    end_port = models.PositiveIntegerField()
-    flag = models.CharField(max_length=255)  # Increased max_length for hashed value
-    point = models.PositiveIntegerField()
-
+    docker_port = models.IntegerField()
+    start_port = models.IntegerField()
+    end_port = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-created_at']
-        verbose_name = 'Challenge'
-        verbose_name_plural = 'Challenges'
-
+    flag = models.CharField(max_length=100)
+    point = models.IntegerField()
+    
     def __str__(self):
         return self.name
-
-    def clean(self):
-        if self.start_port > self.end_port:
-            raise ValidationError(_("Start port must be less than or equal to end port."))
-
+    
+    # Overriding default save method
     def save(self, *args, **kwargs):
-        self.full_clean()  # Ensure clean() is called before saving
-        if self.flag and not self.flag.startswith("hashed_"):
-            self.flag = "hashed_" + hashlib.sha256(self.flag.encode('utf-8')).hexdigest()
-        super().save(*args, **kwargs)
-
+        if self.start_port > self.end_port:
+            raise ValidationError("Start port should be less than end port") # Raise ValidationError if start_port is greater than end_port
+        if self.flag:
+            if not self.flag.startswith("hashed_"):
+                self.flag = "hashed_" + hashlib.sha256(self.flag.encode('utf-8')).hexdigest()
+        super(Challenge, self).save(*args, **kwargs)
 
 class UserChallenge(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_challenges')
-    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name='user_challenges')
+    """
+    This is a mapping of user to challenge with proper progress tracking 
+    This also allows us to reuse the created container for the user
+    """
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
     container_id = models.CharField(max_length=100)
-    port = models.PositiveIntegerField()
-    is_live = models.BooleanField(default=False)
-    no_of_attempt = models.PositiveIntegerField(default=0)
-    is_solved = models.BooleanField(default=False)
-
+    port = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = ('user', 'challenge')
-        ordering = ['-updated_at']
-        verbose_name = 'User Challenge'
-        verbose_name_plural = 'User Challenges'
-
+    is_live = models.BooleanField(default=False)
+    no_of_attempt = models.IntegerField(default=0)
+    is_solved = models.BooleanField(default=False)
+    
     def __str__(self):
         return f"{self.user.username} - {self.challenge.name}"
